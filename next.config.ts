e1071473407a2df91bc478f661e4c72943d4dd39
Chad1704 <chadjoshkatz@gmail.com>
2025-05-20 19:40:32 -0400

    test

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from 'next';
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? '/websitelaunch' : '',
-  assetPrefix: isProd ? '/websitelaunch/' : '',
+  basePath: process.env.PAGES_BASE_PATH,
 };
 
 export default nextConfig;
+
+
