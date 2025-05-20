b042d43968c8849b272a5374d88adf2a23806d24
Chad1704 <chadjoshkatz@gmail.com>
2025-05-20 19:34:29 -0400

    basepath change?
    plz

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from 'next';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.PAGES_BASE_PATH,
+  basePath: isProd ? '/websitelaunch' : '',
+  assetPrefix: isProd ? '/websitelaunch/' : '',
 };
 
 export default nextConfig;
