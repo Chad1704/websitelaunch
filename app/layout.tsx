bd0e8520f6fca83a2f8ba57e4526db66cb8c4d24
Chad1704 <chadjoshkatz@gmail.com>
2025-05-20 17:40:28 -0400

    testicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/frog2.png" type="image/png" />
+<link rel="icon" href="/frog2.png" type="image/png" />
+
       </head>
       <body>{children}</body>
     </html>
