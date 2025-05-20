8534a1c6969f77e38e5c884fcce220c8d5a6eef3
Chad1704 <chadjoshkatz@gmail.com>
2025-05-20 17:20:54 -0400

    Update projectsData.js

--- app/projectsData.js
+++ app/projectsData.js
@@ -8,6 +8,16 @@ const projectsData = [
     Project_Desc: "A project about creation.",
     Project_Date: "15-05-2025"
   },
+
+    {
+    Project_Id: 2,
+    Project_Name: "Web Portfolio",
+    Project_Tags: ["React", "Node.js", "Html"],
+    Project_Img: "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?fm=jpg&q=60&w=3000",
+    Project_Link: "/404",
+    Project_Desc: "A project about creation.",
+    Project_Date: "15-05-2025"
+  },
   
   
 ];
