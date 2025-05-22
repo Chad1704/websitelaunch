688f90129b6a01ada0506fdc76fa5bee3635927d
Chad1704 <chadjoshkatz@gmail.com>
2025-05-22 01:51:11 -0400

    border color's shift
    looks nicer

--- app/landing.jsx
+++ app/landing.jsx
@@ -15,7 +15,7 @@ function Landing() {
       <div className=" overflow-hidden ">
         <div className="grid grid-cols-10 grid-rows-10 h-screen bg-dark transition duration-400 ease-in-out ">
           {/* ColorTester visible only below lg */}
-          <div className="relative hero-border  flex col-span-9 lg:col-span-6 row-start-1 lg:row-start-1 row-span-5 lg:row-span-7  overflow-hidden">
+          <div className="relative hero-border transition duration-400 ease-in-out  flex col-span-9 lg:col-span-6 row-start-1 lg:row-start-1 row-span-5 lg:row-span-7  overflow-hidden">
             <div className="absolute top-0 right-0 lg:hidden p-2 z-10">
               <ColorTester />
             </div>
@@ -23,7 +23,7 @@ function Landing() {
           </div>
 
           {/* About */}
-          <div className=" flex about-border   col-span-9 lg:col-span-2 row-span-2 lg:row-span-7">
+          <div className=" flex about-border transition duration-400 ease-in-out  col-span-9 lg:col-span-2 row-span-2 lg:row-span-7">
             <div className="grid grid-cols-12  grid-rows-12">
               {/* ColorTester visible only on lg and above */}
               <div className="h-fit w-fit hidden  lg:block">
@@ -42,7 +42,7 @@ function Landing() {
           </div>
 
           {/* Projects */}
-          <div className="overflow-hidden pro-border flex items-center justify-center col-span-9 lg:col-span-8 row-span-3 lg:row-span-4">
+          <div className="overflow-hidden pro-border flex items-center justify-center col-span-9 transition duration-400 ease-in-out lg:col-span-8 row-span-3 lg:row-span-4">
             <ProjectsHub />
           </div>
         </div>
