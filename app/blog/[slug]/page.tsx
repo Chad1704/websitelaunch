44772265e035e722c5f0f2ea944c1de27dd105bf
Chad1704 <157647582+Chad1704@users.noreply.github.com>
2025-05-22 17:28:53 -0400

    blog work

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,18 +1,18 @@
-import Blog from "../blog";
 import PostLayout from "../../postLayout";
-
 import Header from "../../header";
 
 export default async function Page({ params }: { params: { slug: string } }) {
   const { slug } = params;
-  const { default: Post } = await import(`./posts/${slug}.mdx`);
+  const postModule = await import(`./posts/${slug}.mdx`);
+  const Post = postModule.default;
+  const metadata = postModule.metadata;
+
   return (
     <>
       <Header />
-
       <div className="grid grid-cols-12">
-        <div className="col-start-1 col-span-12 ">
-          <PostLayout>
+        <div className="col-start-1 col-span-12">
+          <PostLayout tags={metadata.tags} title={metadata.title}>
             <Post />
           </PostLayout>
         </div>
--- app/postLayout.jsx
+++ app/postLayout.jsx
@@ -2,13 +2,28 @@
 import React from "react";
 import "./blog/blog.css";
 
-export default function PostLayout({ children }) {
+export default function PostLayout({ children, tags = [], title }) {
   return (
     <div className="grid grid-cols-12 min-h-screen bg-dark text-light transition duration-400 ease-in-out">
-      <article className="prose col-span-9 lg:prose-xl ">
+      <article className="prose col-span-9 lg:prose-xl">
         <div className="px-20 pt-10 blog-post">{children}</div>
       </article>
-      <div className="col-span-3 card-head-side-l ">Info / Tree</div>
+
+      <div className="col-span-3 card-head-side-l">
+        <aside className="  p-6 bg-red-500 h-fit">
+          <h3 className="text-xl font-bold mb-2">Tags</h3>
+          <div className=" text-sm text-gray-300">
+            {tags.map((tag, idx) => (
+              <div
+                key={idx}
+                className="inline-flex items-center px-3 py-1 mr-2 mb-2 rounded-full border border-light text-light text-sm pluto transition duration-400 ease-in-out"
+              >
+                {tag}
+              </div>
+            ))}
+          </div>
+        </aside>
+      </div>
     </div>
   );
 }
