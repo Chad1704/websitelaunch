35a82de73b2a123a5aaae1c382a615893756a20d
Chad1704 <157647582+Chad1704@users.noreply.github.com>
2025-05-22 15:13:27 -0400

    test

--- app/projectsData.js
+++ app/projectsData.js
@@ -3,23 +3,56 @@ const projectsData = [
     Project_Id: 1,
     Project_Name: "Web Portfolio",
     Project_Tags: ["React", "Node.js", "Html"],
-    Project_Img: "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?fm=jpg&q=60&w=3000",
+    Project_Img:
+      "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?fm=jpg&q=60&w=3000",
     Project_Link: "/404",
     Project_Desc: "A project about creation.",
-    Project_Date: "15-05-2025"
+    Project_Date: "15-05-2025",
   },
 
-    {
+  {
     Project_Id: 2,
     Project_Name: "Breadcrums",
     Project_Tags: ["Maps Api", "Data Stuctures", "Full Stack"],
-    Project_Img: "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?fm=jpg&q=60&w=3000",
+    Project_Img:
+      "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?fm=jpg&q=60&w=3000",
     Project_Link: "/404",
     Project_Desc: "A project about creation.",
-    Project_Date: "15-05-2025"
+    Project_Date: "15-05-2025",
   },
-  
-  
+
+  {
+    Project_Id: 3,
+    Project_Name: "Hi mom",
+    Project_Tags: ["hi mom ", "Data Stuctures", "Full Stack"],
+    Project_Img:
+      "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?fm=jpg&q=60&w=3000",
+    Project_Link: "/404",
+    Project_Desc: "A project about creation.",
+    Project_Date: "15-05-2025",
+  },
+
+  {
+    Project_Id: 4,
+    Project_Name: "Breadcrums",
+    Project_Tags: ["Maps Api", "Data Stuctures", "Full Stack"],
+    Project_Img:
+      "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?fm=jpg&q=60&w=3000",
+    Project_Link: "/404",
+    Project_Desc: "A project about creation.",
+    Project_Date: "15-05-2025",
+  },
+
+  {
+    Project_Id: 5,
+    Project_Name: "Breadcrums",
+    Project_Tags: ["Maps Api", "Data Stuctures", "Full Stack"],
+    Project_Img:
+      "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?fm=jpg&q=60&w=3000",
+    Project_Link: "/404",
+    Project_Desc: "A project about creation.",
+    Project_Date: "15-05-2025",
+  },
 ];
 
 export default projectsData;
