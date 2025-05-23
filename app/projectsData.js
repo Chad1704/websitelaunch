cf0054a3dee17e4a382ad4404377cdd7e4272f70
Chad1704 <157647582+Chad1704@users.noreply.github.com>
2025-05-22 22:31:53 -0400

    Update projectsData.js

--- app/projectsData.js
+++ app/projectsData.js
@@ -31,6 +31,17 @@ const projectsData = [
     Project_Desc: "A project about creation.",
     Project_Date: "15-05-2025",
   },
+
+  {
+    Project_Id: 3,
+    Project_Name: "PUSHHHHHHHHHHHHHHHHHHHHHHHHH",
+    Project_Tags: ["hi mom ", "Data Stuctures", "Full Stack"],
+    Project_Img:
+      "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?fm=jpg&q=60&w=3000",
+    Project_Link: "/404",
+    Project_Desc: "A project about creation.",
+    Project_Date: "15-05-2025",
+  },
 ];
 
 export default projectsData;
