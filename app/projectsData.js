cc8631de82fc39d7a5a98ef76bc52b94a145e174
Chad1704 <157647582+Chad1704@users.noreply.github.com>
2025-05-23 03:01:03 -0400

    blog update

--- app/projectsData.js
+++ app/projectsData.js
@@ -2,10 +2,10 @@ const projectsData = [
   {
     Project_Id: 1,
     Project_Name: "Web Portfolio",
-    Project_Tags: ["React", "Node.js", "Html"],
+    Project_Tags: ["Html", "Java", "Design", "Next.js", "React"],
     Project_Img:
       "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?fm=jpg&q=60&w=3000",
-    Project_Link: "/404",
+    Project_Link: "http://localhost:3000/blog/BlogLaunchPost",
     Project_Desc: "A project about creation.",
     Project_Date: "15-05-2025",
   },
